3f3756a02189e7a846128e569204c27b9cc7c51c
Thomas Proisl <thomas.proisl@fau.de>
2018-10-10 09:51:53 +0200

    Replace is not in-place…

--- complexity_measures/utils.py
+++ complexity_measures/utils.py
@@ -101,7 +101,7 @@ def read_txt_csv_graphs(f):
                 g.node[tid]["root"] = "root"
             else:
                 g.add_edge(gov, tid, relation=rel)
-            tree_frag.replace("*", "(%s %s)" % (token[9], token[6]))
+            tree_frag = tree_frag.replace("*", "(%s %s)" % (token[9], token[6]))
             tree.append(tree_frag)
         tree = "".join(tree)
         sensible, explanation = is_sensible_graph(g)
