b1deb1c79a3e567018eb7716bf043abba8f3ba8e
Thomas Proisl <thomas.proisl@fau.de>
2018-10-10 09:37:55 +0200

    Functions for new input format

--- complexity_measures/utils.py
+++ complexity_measures/utils.py
@@ -58,6 +58,59 @@ def read_jtf_graphs(f):
             logging.warn("%s. Ignoring sentence with ID %s." % (explanation, sentence_id))
 
 
+def read_txt_csv_sentences(f):
+    """Read a file in txt.csv format, i.e. a tab-separated file with 21
+    columns.
+
+    """
+    header = f.readline().rstrip().split("\t")
+    assert header == "SectionId ParagraphId SentenceId TokenId Begin End Token Lemma CPOS POS Chunk Morphology Hyphenation DependencyHead DependencyRelation NamedEntity QuoteMarker CoreferenceChainIds SyntaxTree Predicate SemanticArgumentIndex".split()
+    sentence_id = None
+    sentence = []
+    for line in f:
+        fields = line.rstrip().split("\t")
+        if fields[2] != sentence_id:
+            if sentence_id is not None:
+                yield sentence
+                sentence = []
+            sentence_id = fields[2]
+        sentence.append(fields)
+    yield sentence
+
+
+def read_txt_csv_graphs(f):
+    """Read a file in txt.csv format, i.e. a tab-separated file with 21
+    columns, convert the sentences to networkx graphs and reconstruct
+    the phrase structure tree.
+
+    """
+    def attributes(t):
+        return int(t[3]), {"token": t[6], "lemma": t[7], "cpos": t[8], "pos": t[9], "morphology": t[11]}
+    sentences = read_jtf_sentences(f)
+    for sentence in sentences:
+        sentence_id = sentence[0][2]
+        g = networkx.DiGraph(sentence_id=sentence_id)
+        g.add_nodes_from([attributes(t) for t in sentence])
+        tree = []
+        for token in sentence:
+            tid = token[3]
+            gov = token[13]
+            rel = token[14]
+            tree_frag = token[18]
+            if gov == "-1":
+                g.node[tid]["root"] = "root"
+            else:
+                g.add_edge(gov, tid, relation=rel)
+            tree_frag.replace("*", "(%s %s)" % (token[9], token[6]))
+            tree.append(tree_frag)
+            tree = "".join(tree)
+        sensible, explanation = is_sensible_graph(g)
+        if sensible:
+            yield g, tree
+        else:
+            logging.warn("%s. Ignoring sentence with ID %s." % (explanation, sentence_id))
+
+
 def is_sensible_graph(g):
     """Check if g is a sensible syntactic representation of a sentence,
     i.e. rooted, connected, …
