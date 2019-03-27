4cf655025d6fc29c1e18fea9d369620c72b8539b
Thomas Proisl <thomas.proisl@fau.de>
2019-03-27 08:43:53 +0100

    Script for computing complexity measures from the command line

---
+++ bin/lascomplexity.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from complexity_measures import dependency_based
+from complexity_measures import constituent_based
+from complexity_measures import vocabulary_richness
+from complexity_measures import utils
+
+
+def arguments():
+    parser = argparse.ArgumentParser(description="An averaged perceptron part-of-speech tagger")
+    parser.add_argument("-v", "--voc", action="store_true", help="Compute vocabulary-based complexity measures")
+    parser.add_argument("-d", "--dep", action="store_true", help="Compute dependency-based complexity measures")
+    parser.add_argument("-c", "--const", action="store_true", help="Compute constituent-based complexity measures")
+    parser.add_argument("TEXT", type=argparse.FileType("r", encoding="utf-8"), help="Input file. Path to a file or "-" for STDIN. A CoNLL-style text file with six tab-separated columns and an empty line after each sentence. The columns are: word index, word, part-of-speech tag, index of dependency head, dependency relation, phrase structure tree. Missing values can be replaced with an underscore (_).")
+    return parser.parse_args()
+
+
+def vocabulary_measures(tokens):
+    """"""
+    lexical = ["type_token_ratio", "guiraud_r", "herdan_c",
+               "dugast_k", "maas_a2", "dugast_u", "tuldava_ln",
+               "brunet_w", "cttr", "summer_s", "sichel_s",
+               "michea_m", "honore_h", "herdan_vm", "entropy",
+               "yule_k", "simpson_d", "hdd", "mtld"]
+    word_length = [vocabulary_richness.average_token_length_characters,
+                   vocabulary_richness.average_token_length_syllables]
+    for measure in lexical:
+        score, ci = vocabulary_richness.bootstrap(tokens, measure=measure, window_size=5000, ci=True)
+        print("", score, ci, sep="\t", end="")
+    for wl in word_length:
+        score, stdev = wl(tokens)
+        print("", score, stdev, sep="\t", end="")
+
+
+def dependency_measures(graphs):
+    """"""
+    measures = [dependency_based.average_average_dependency_distance,
+                dependency_based.average_closeness_centrality,
+                dependency_based.average_outdegree_centralization,
+                dependency_based.average_closeness_centralization,
+                dependency_based.average_sentence_length,
+                dependency_based.average_sentence_length_characters,
+                dependency_based.average_sentence_length_syllables,
+                dependency_based.average_dependents_per_word,
+                dependency_based.average_longest_shortest_path,
+                dependency_based.average_punctuation_per_sentence]
+    for measure in measures:
+        score, stdev = measure(graphs)
+        print("", score, stdev, sep="\t", end="")
+
+
+def constituent_measures(trees):
+    """"""
+    measures = [constituent_based.average_t_units,
+                constituent_based.average_complex_t_units,
+                constituent_based.average_clauses,
+                constituent_based.average_dependent_clauses,
+                constituent_based.average_nps,
+                constituent_based.average_vps,
+                constituent_based.average_pps,
+                constituent_based.average_coordinate_phrases,
+                constituent_based.average_constituents,
+                constituent_based.average_constituents_wo_leaves,
+                constituent_based.average_height]
+    for measure in measures:
+        result = measure(trees)
+        if isinstance(result, tuple):
+            result = "\t".join(str(r) for r in result)
+        print("", result, sep="\t", end="")
+
+
+def main():
+    args = arguments()
+    if not any((args.voc, args.dep, args.const)):
+        args.voc = True
+        args.dep = True
+        args.const = True
+    tokens, graphs, trees = zip(*utils.read_tsv(args.TEXT, args.voc, args.dep, args.const))
+    vocabulary_measures(tokens)
+    dependency_measures(graphs)
+    constituent_measures(trees)
+    header = "id filename genre type_token_ratio type_token_ratio_ci guiraud_r guiraud_r_ci herdan_c herdan_c_ci dugast_k dugast_k_ci maas_a2 maas_a2_ci dugast_u dugast_u_ci tuldava_ln tuldava_ln_ci brunet_w brunet_w_ci cttr cttr_ci summer_s summer_s_ci sichel_s sichel_s_ci michea_m michea_m_ci honore_h honore_h_ci herdan_vm herdan_vm_ci entropy entropy_ci yule_k yule_k_ci simpson_d simpson_d_ci hdd hdd_ci mtld mtld_ci word_length_char word_length_char_stdev word_length_syll word_length_syll_stdev dependency_distance dependency_distance_stdev closeness_centrality closeness_centrality_stdev outdegree_centralization outdegree_centralization_stdev closeness_centralization closeness_centralization_stdev average_sentence_length average_sentence_length_stdev average_sentence_length_char average_sentence_length_char_stdev average_sentence_length_syll average_sentence_length_syll_stdev dependents_per_word dependents_per_word_stdev longest_shortest_path longest_shortest_path_stdev punctuation_per_sentence punctuation_per_sentence_stdev t_units t_units_stdev t_units_length t_units_length_stdev complex_t_units complex_t_units_stdev complex_t_units_length complex_t_units_length_stdev clauses clauses_stdev clauses_length clauses_length_stdev dependent_clauses dependent_clauses_stdev dependent_clauses_length dependent_clauses_length_stdev nps nps_stdev nps_length nps_length_stdev vps vps_stdev vps_length vps_length_stdev pps pps_stdev pps_length pps_length_stdev coordinate_phrases coordinate_phrases_stdev coordinate_phrases_length coordinate_phrases_length_stdev constituents constituents_stdev constituents_wo_leaves constituents_wo_leaves_stdev height height_stdev".split()
+
+
+if __name__ == "__main__":
+    main()
