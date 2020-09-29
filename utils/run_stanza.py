5aae8f5be9c8ec85790f73ffb029b0d8b604dbfa
Thomas Proisl <thomas.proisl@fau.de>
2020-09-29 11:51:02 +0200

    Utility script for parsing texts with stanza

---
+++ utils/run_stanza.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+
+import stanza
+from stanza.utils.conll import CoNLL
+import stanza.resources.common
+
+
+def arguments():
+    stanza_resources = json.load(open(os.path.join(stanza.resources.common.DEFAULT_MODEL_DIR, "resources.json")))
+    parser = argparse.ArgumentParser(description="Parse input texts to CONLL-U format using stanza.")
+    parser.add_argument("-l", "--language", choices=sorted(stanza_resources.keys()), required=True, help="Input language.")
+    parser.add_argument("-o", "--output-dir", type=os.path.abspath, default=".", help="Output directory. Default: Current directory.")
+    parser.add_argument("TEXT", type=argparse.FileType("r", encoding="utf-8"), nargs="+", help="Input text files. Paths to files or \"-\" for STDIN.")
+    return parser.parse_args()
+
+
+def main():
+    args = arguments()
+    stanza.download(args.language)
+    nlp = stanza.Pipeline(args.language, processors="tokenize,mwt,pos,lemma,depparse")
+    for fh in args.TEXT:
+        filename = os.path.basename(fh.name)
+        text = fh.read()
+        doc = nlp(text)
+        dicts = doc.to_dict()
+        conll = CoNLL.convert_dict(dicts)
+        with open(os.path.join(args.output_dir, filename + ".conllu"), mode="w", encoding="utf-8") as out:
+            for sentence in conll:
+                out.write("\n".join(("\t".join(token) for token in sentence)))
+                out.write("\n\n")
+
+
+if __name__ == "__main__":
+    main()
